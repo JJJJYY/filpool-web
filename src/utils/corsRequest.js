e8b13b9555dc543cfc25ee18d6ac06ac21b43e6c
JJJJYY <843418890@qq.com>
2020-09-23 17:53:52 +0800

    注释原生XML请求

--- src/utils/corsRequest.js
+++ src/utils/corsRequest.js
@@ -21,57 +21,57 @@ import router from '../router/index';
 
 export default (params, callback) => {
   return new Promise((resolve, reject) => {
-    // plusReady(() => {
-    // let xhr = new window.plus.net.XMLHttpRequest();
-    let xhr = new XMLHttpRequest();
-    console.log('xhr', xhr)
-    // console.log(params)
-    let type = params.type && params.type.toUpperCase() || 'POST';
-    let url = params.url;
-    let reqParams = params.data || null;
-    // let targetUrl = serviceURL;
-    // let url = params.isRelative ? params.url : `${targetUrl}${params.url}`;
-    // if (type === "GET") {
-    //   url += `?${stringify(reqParams)}`;
-    // } else {
-    reqParams = JSON.stringify(reqParams);
-    // }
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        let res = JSON.parse(xhr.responseText);
-        if (xhr.status === 200 || xhr.status === 304) {
-          if (res.ret === 200) {
-            resolve(res);
-          } else if (res.ret === 401) {
-            Toast(res.msg)
-            router.push({
-              path: '/login'
-            })
+    plusReady(() => {
+      let xhr = new window.plus.net.XMLHttpRequest();
+      // let xhr = new XMLHttpRequest();
+      // console.log('xhr', xhr)
+      // console.log(params)
+      let type = params.type && params.type.toUpperCase() || 'POST';
+      let url = params.url;
+      let reqParams = params.data || null;
+      // let targetUrl = serviceURL;
+      // let url = params.isRelative ? params.url : `${targetUrl}${params.url}`;
+      // if (type === "GET") {
+      //   url += `?${stringify(reqParams)}`;
+      // } else {
+      reqParams = JSON.stringify(reqParams);
+      // }
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          let res = JSON.parse(xhr.responseText);
+          if (xhr.status === 200 || xhr.status === 304) {
+            if (res.ret === 200) {
+              resolve(res);
+            } else if (res.ret === 401) {
+              Toast(res.msg)
+              router.push({
+                path: '/login'
+              })
+            } else {
+              Toast(res.msg || '请求服务器失败');
+              reject();
+            }
           } else {
-            Toast(res.msg || '请求服务器失败');
-            reject();
+            Toast("请求服务器失败");
           }
-        } else {
-          Toast("请求服务器失败");
+          typeof callback === 'function' && callback(res)
         }
-        typeof callback === 'function' && callback(res)
-      }
-    };
-    xhr.open(type, url);
-    // if (type === 'POST') {
-    //   let contentType = params['Content-Type'] || 'application/json';
-    //   xhr.setRequestHeader('Content-Type', contentType);
-    // }
-    // if (params.config && params.config.headers) {
-    //   if (params.config.responseType) {
-    //     xhr.responseType = params.config.responseType;
-    //   }
-    //   for (let field in params.config.headers) {
-    //     xhr.setRequestHeader(field, params.config.headers[field]);
-    //   }
-    // }
+      };
+      xhr.open(type, url);
+      // if (type === 'POST') {
+      //   let contentType = params['Content-Type'] || 'application/json';
+      //   xhr.setRequestHeader('Content-Type', contentType);
+      // }
+      // if (params.config && params.config.headers) {
+      //   if (params.config.responseType) {
+      //     xhr.responseType = params.config.responseType;
+      //   }
+      //   for (let field in params.config.headers) {
+      //     xhr.setRequestHeader(field, params.config.headers[field]);
+      //   }
+      // }
 
-    xhr.send(reqParams);
-    // })
+      xhr.send(reqParams);
+    })
   })
 }
