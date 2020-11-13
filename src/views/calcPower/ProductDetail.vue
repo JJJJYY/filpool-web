959094516d4babb13a31df10c9e1263b9483e57f
JJJJYY <843418890@qq.com>
2020-11-13 11:35:12 +0800

    详情页ui

--- src/views/calcPower/ProductDetail.vue
+++ src/views/calcPower/ProductDetail.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="ProductDetail">
-    <head-nav></head-nav>
+    <div class="imageSize">
+      <head-nav class="headNavStyle"></head-nav>
+    </div>
     <div class="container list page-container">
+      <!-- <img src="../../assets/img/smallBanner.png" class="imageSize" alt="" /> -->
       <div class="goodBox">
         <CalcPowerItem
           v-if="detailInfo"
@@ -11,83 +14,85 @@
           :show="true"
         />
       </div>
-      <van-tabs v-model="active" color="#ff933c">
-        <van-tab title="排队列表">
-          <van-list
-            v-model="loading"
-            :finished="finished"
-            finished-text="没有更多了"
-            @load="onLoad"
-          >
-            <div class="vanTab-r" v-for="(item, index) in list" :key="index">
-              <div class="vanTab-padding">
-                <div class="vanTab-flex">
-                  <div>排序： {{ index + 1 }}</div>
-                  <div>数量： {{ item.power | parseFloatFilter }}TiB</div>
-                </div>
-                <div class="vanTab-flex">
-                  <div>ID： {{ item.auth_user_id }}</div>
-                  <div style="color: #666666 ; font-size: 12px">
-                    {{ item.create_time }}
+      <div class="listBox">
+        <van-tabs v-model="active" type="card" color="#ff933c">
+          <van-tab title="排队列表">
+            <van-list
+              v-model="loading"
+              :finished="finished"
+              finished-text="没有更多了"
+              @load="onLoad"
+            >
+              <div class="vanTab-r" v-for="(item, index) in list" :key="index">
+                <div class="vanTab-padding">
+                  <div class="vanTab-flex">
+                    <div>排序： {{ index + 1 }}</div>
+                    <div>数量： {{ item.power | parseFloatFilter }}TiB</div>
+                  </div>
+                  <div class="vanTab-flex">
+                    <div>ID： {{ item.auth_user_id }}</div>
+                    <div style="color: #666666 ; font-size: 12px">
+                      {{ item.create_time }}
+                    </div>
                   </div>
                 </div>
               </div>
+            </van-list>
+          </van-tab>
+          <van-tab title="详细规则">
+            <div class="detail">
+              <p class="label">具体细则</p>
+              <div class="content">
+                <p>
+                  1.FILPool矿池“算力加速计划”-期初定于11月10日开始实施，将采用新的独立节点运行，新节点有效算力及挖矿收益并入FILPool-期矿池，FILPool-期矿池总存储容量不再增加，每名用户将根据其账户有效算力占矿池总有效算力占比来分配收益;
+                </p>
+                <br />
+                <p>
+                  2.“算力加速计划”每天限量申请1PiB (1024TiB)
+                  ，1TiB有效算力所需质押币按6.4枚FIL计算，最小申请单位为0.001TiB,
+                  11月9日12:00开放申请，11月10日开启新节点。FILPool矿池用户可申请(充值)
+                  FIL数量的.
+                  上限以其所购买的存储空间剩余所未封装扇区所需要的质押币为，上限，如用户充值的FIL超过质押所需要的FIL,多余部分再算力加速计划结束之后自动转至其“可用资产”账户，用户可申请提现;
+                  举例说明:客户A购买了-台存储容量为192TB蜘蛛矿机，其有效算力上限为128TiB,目前已完成封装的有效算力约为28TiB,还需封装100TiB的有效算力，共还需质押640枚FIL，也就是其最高可申请(充值)的FIL数量为640枚。
+                </p>
+
+                <br />
+                <p>
+                  3.参与申请“算力加速计划”的用户，则其账户在申请后第三天对应增加其所申请的有效算力。因矿池每日新增有效算力会存在-定波动如新节点每天有效算力增长未达到预期值，则按比例分配有效算力，不足部分后续补足;用户有效算力达到上线后，每日挖矿收益中可用部分均可全部提取。
+                </p>
+
+                <br />
+                <p>
+                  4.未参与“算力加速计划”的用户且未将其可用资产提取，则其有效算力将根据FILPool-期矿池原有节点“f02770、
+                  f02775、
+                  f02767”每日新增有效算力与其个人账户剩余可用资产及对应比例进行同步增长;如用户未参与“算力加速计划”并且将当日可用资产全部提取，则其有效算力当日不再增长，如后续其暂停提币，则其有效算力继续增长;
+                </p>
+
+                <br />
+                <p>
+                  5.“算力加速计划”暂定截止时间为FILPool-
+                  期矿池达到有效算力。上限为止;
+                </p>
+
+                <br />
+                <p>
+                  6.目前Fil
+                  ecoin挖矿所采用的扇区生命周期默认为540天，即质押币质押周期为540天，质押期到期后质押币一次性释放。
+                  如用户所购买的服务器托管服务或存储空间租赁服务一年到期后不续约，FILPool矿
+                  池可回购用户账户的有效算力，提前给用户兑付释放质押币。
+                </p>
+              </div>
             </div>
-          </van-list>
-        </van-tab>
-        <!-- <van-tab title="详细规则">
-          <div class="detail">
-            <p class="label">具体细则</p>
-            <div class="content">
-              <p>
-                1.FILPool矿池“算力加速计划”-期初定于11月10日开始实施，将采用新的独立节点运行，新节点有效算力及挖矿收益并入FILPool-期矿池，FILPool-期矿池总存储容量不再增加，每名用户将根据其账户有效算力占矿池总有效算力占比来分配收益;
-              </p>
-              <br />
-              <p>
-                2.“算力加速计划”每天限量申请1PiB (1024TiB)
-                ，1TiB有效算力所需质押币按6.4枚FIL计算，最小申请单位为0.001TiB,
-                11月9日12:00开放申请，11月10日开启新节点。FILPool矿池用户可申请(充值)
-                FIL数量的.
-                上限以其所购买的存储空间剩余所未封装扇区所需要的质押币为，上限，如用户充值的FIL超过质押所需要的FIL,多余部分再算力加速计划结束之后自动转至其“可用资产”账户，用户可申请提现;
-                举例说明:客户A购买了-台存储容量为192TB蜘蛛矿机，其有效算力上限为128TiB,目前已完成封装的有效算力约为28TiB,还需封装100TiB的有效算力，共还需质押640枚FIL，也就是其最高可申请(充值)的FIL数量为640枚。
-              </p>
-
-              <br />
-              <p>
-                3.参与申请“算力加速计划”的用户，则其账户在申请后第三天对应增加其所申请的有效算力。因矿池每日新增有效算力会存在-定波动如新节点每天有效算力增长未达到预期值，则按比例分配有效算力，不足部分后续补足;用户有效算力达到上线后，每日挖矿收益中可用部分均可全部提取。
-              </p>
-
-              <br />
-              <p>
-                4.未参与“算力加速计划”的用户且未将其可用资产提取，则其有效算力将根据FILPool-期矿池原有节点“f02770、
-                f02775、
-                f02767”每日新增有效算力与其个人账户剩余可用资产及对应比例进行同步增长;如用户未参与“算力加速计划”并且将当日可用资产全部提取，则其有效算力当日不再增长，如后续其暂停提币，则其有效算力继续增长;
-              </p>
-
-              <br />
-              <p>
-                5.“算力加速计划”暂定截止时间为FILPool-
-                期矿池达到有效算力。上限为止;
-              </p>
-
-              <br />
-              <p>
-                6.目前Fil
-                ecoin挖矿所采用的扇区生命周期默认为540天，即质押币质押周期为540天，质押期到期后质押币一次性释放。
-                如用户所购买的服务器托管服务或存储空间租赁服务一年到期后不续约，FILPool矿
-                池可回购用户账户的有效算力，提前给用户兑付释放质押币。
-              </p>
-            </div>
-          </div>
-        </van-tab> -->
-      </van-tabs>
-      <van-popup
-        v-model="show"
-        position="bottom"
-        closeable
-        :safe-area-inset-bottom="true"
-      >
-      </van-popup>
+          </van-tab>
+        </van-tabs>
+        <van-popup
+          v-model="show"
+          position="bottom"
+          closeable
+          :safe-area-inset-bottom="true"
+        >
+        </van-popup>
+      </div>
     </div>
   </div>
 </template>
@@ -178,10 +183,62 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.imageSize {
+  height: 165px;
+  background-image: url("../../assets/img/smallBanner.png");
+  background-size: 100% 100%;
+}
 .ProductDetail {
-  .container {
-    /*height: calc(100vh - 62px);
-    overflow: auto;*/
+  background: #1f1f1e;
+  .headNavStyle {
+    /deep/.van-nav-bar {
+      background: #1f1f1e !important;
+      &:after {
+        border: none;
+      }
+      .van-nav-bar__left {
+        img {
+          filter: brightness(100);
+        }
+      }
+      .van-ellipsis {
+        .title {
+          color: #fff;
+        }
+      }
+    }
+  }
+  .goodBox {
+    padding: 0 10px;
+  }
+  .listBox {
+    margin: 0 10px;
+    background: #fff;
+    border-radius: 8px;
+    /deep/.van-tabs {
+      .van-tabs__nav--card .van-tab {
+        border-right: none;
+      }
+      .van-tabs__wrap {
+        height: 50px;
+        .van-tabs__nav {
+          height: 50px;
+          border: none;
+          .van-tab {
+            .van-tab__text {
+              color: #666666ff;
+            }
+          }
+          .van-tab--active {
+            background: none !important;
+
+            .van-tab__text {
+              color: #f9a03eff;
+            }
+          }
+        }
+      }
+    }
   }
   .detail {
     padding: 16px;
@@ -203,6 +260,7 @@ export default {
 .vanTab-r {
   // padding: 0 10px;
   margin-top: 8px;
+  border-bottom: 1px solid #e8e8e8ff;
   .vanTab-padding {
     border-radius: 8px;
     background: #fff;
